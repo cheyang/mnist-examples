6cc5021d69e26c00413e9d555286cd70f554ec79
必嘫 <cheyang.cy@alibaba-inc.com>
2017-05-22 18:15:46 +0800

    mnist train

--- mnist_train.py
+++ mnist_train.py
@@ -123,7 +123,7 @@ def run_training():
   # Tell TensorFlow that the model will be built into the default Graph.
   with tf.Graph().as_default():
     # Input images and labels.
-    image, label = inputs()
+    image, label = inputs(FLAGS.num_epochs)
 
     # Build a Graph that computes predictions
     x = tf.placeholder(dtype=tf.float32, shape=[None, 784], name='x-input')
